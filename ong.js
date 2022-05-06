c03715bf0da4682d1127868dcda2c451e6952aff
gbHenderson <102072061+gbHenderson@users.noreply.github.com>
2022-05-06 15:09:04 -0300

    ajustes finais

--- ong.js
+++ ong.js
@@ -2,8 +2,8 @@ function menu(){
     var menu = document.getElementsByTagName('div')[1]
     
     if(menu.classList.contains('show')){
-        menu.classList.add('hide')
-        menu.classList.remove('show')}
+       menu.classList.add('hide')
+       menu.classList.remove('show')}
 
     else{menu.classList.add('show')
-        menu.classList.remove('hide')}}
+         menu.classList.remove('hide')}}
